3449b7bde9db58a2572b74ba061b95717add75aa
subdavis <bdavis@redspin.net>
2016-07-27 22:26:29 -0400

    exec magically works now

--- i3status.sh
+++ i3status.sh
@@ -7,9 +7,9 @@ while true; do
         home=$(df -h | awk '/\/dev\/sda4/ {printf "Home: %s", $5}')
         now=$(date +%Y-%m-%d\ %I:%M:%S)
         cpu=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')
-        mem=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
+        mem=$(free | grep Mem | awk '{printf "%.2f", ($3/$2 * 100.0)}')
         wifi=$(iwconfig enx048d39367326 | awk '/Signal\ level/ {printf "%s %s", $3, $4}' | sed 's:[a-zA-Z\ ]*=::')
         rxtx=$(ifconfig enx048d39367326 | grep 'bytes' | awk '/RX/ {printf "R: %s %s T: %s %s", $3, $4, $7, $8}')
-        echo " $boot | $home | $rxtx | CPU: $cpu | Mem(used): $mem% | Signal: $wifi | $now " || exit 1
+        echo " $boot   |   $home   |   $rxtx   |   CPU: $cpu   |   Mem(used): $mem%   |   Signal: $wifi   |   $now " || exit 1
         sleep 2s
 done
